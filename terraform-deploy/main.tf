2671af056886ecc2d7d70d725abf3cf0f16f0e53
jsrayanyaqoob <rayanyaqoob83@gmail.com>
2026-07-13 17:05:38 +0500

    Updated

--- terraform-deploy/main.tf
+++ terraform-deploy/main.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = ">= 1.5"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -12,44 +13,26 @@ provider "aws" {
 }
 
 variable "ami_id" {
-  description = "ami-0fc17d97b67068516"
-  type        = string
+  type = string
 }
 
-# Reference your EXISTING launch template (doesn't create a new one)
-data "aws_launch_template" "interviewarena" {
-  name = "interviewarena-launch-template"
+data "aws_launch_template" "app" {
+  name = "interviewname-launch-template"
 }
 
-# Create a new version of the launch template with the new AMI
-resource "aws_launch_template" "interviewarena_new_version" {
-  name_prefix = "interviewarena-launch-template"
-  image_id    = var.ami_id
-
-  # Reuse settings from the existing template
-  instance_type = data.aws_launch_template.interviewarena.instance_type
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-# Reference your EXISTING ASG
-data "aws_autoscaling_group" "interviewarena" {
-  name = "interviewarena-asg"
-}
-
-# Trigger an instance refresh whenever the AMI changes
-resource "null_resource" "instance_refresh" {
+resource "null_resource" "deploy" {
   triggers = {
     ami_id = var.ami_id
   }
 
   provisioner "local-exec" {
+    interpreter = ["PowerShell", "-Command"]
     command = <<-EOT
-      aws autoscaling start-instance-refresh \
-        --auto-scaling-group-name interviewarena-asg \
-        --preferences '{"MinHealthyPercentage": 50, "InstanceWarmup": 60}'
+      $NEW_VERSION = aws ec2 create-launch-template-version --launch-template-id ${data.aws_launch_template.app.id} --source-version ${data.aws_launch_template.app.latest_version} --launch-template-data '{\"ImageId\":\"${var.ami_id}\"}' --query "LaunchTemplateVersion.VersionNumber" --output text
+
+      aws ec2 modify-launch-template --launch-template-id ${data.aws_launch_template.app.id} --default-version $NEW_VERSION
+
+      aws autoscaling start-instance-refresh --auto-scaling-group-name interviewarena-asg --preferences MinHealthyPercentage=50,InstanceWarmup=90
     EOT
   }
 }
